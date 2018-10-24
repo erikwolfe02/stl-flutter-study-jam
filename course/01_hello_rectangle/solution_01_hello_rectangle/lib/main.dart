11179b5cb3f335d2295a5a3d466550011d0c0b9a
Erik Wolfe <me@erikwolfe.com>
2018-10-23 22:48:39 -0700

    Adding light customizations

--- course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
+++ course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
@@ -4,6 +4,18 @@
 
 import 'package:flutter/material.dart';
 
+class SomeStatefullWidget extends StatefulWidget {
+  @override
+  _SomeStatefullWidgetState createState() => _SomeStatefullWidgetState();
+}
+
+class _SomeStatefullWidgetState extends State<SomeStatefullWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 void main() {
   runApp(
     MaterialApp(
@@ -24,17 +36,51 @@ class HelloRectangle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        color: Colors.greenAccent,
+        decoration: _addGradientDecoration(),
+//            color: Colors.greenAccent,
         height: 400.0,
         width: 300.0,
         child: Center(
           child: Text(
             'Hello!',
-            style: TextStyle(fontSize: 40.0),
+            style: TextStyle(fontSize: 40.0, color: Colors.white),
             textAlign: TextAlign.center,
           ),
         ),
       ),
     );
   }
+
+  BoxDecoration _addGradientDecoration() => BoxDecoration(
+//        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment(0.8, 0.0),
+          // 10% of the width, so there are ten blinds.
+          colors: [const Color(0xFF69F0AE), const Color(0xFF00C853)],
+          // whitish to gray
+          tileMode: TileMode.clamp, // repeats the gradient over the canvas
+        ),
+      );
 }
+
+//RaisedButton(
+//onPressed: () => print("on pressed"),
+//child: Text(
+//"Button",
+//),
+//),
+
+//Padding(
+//padding: const EdgeInsets.all(8.0),
+//child: Container(
+//decoration: _addGradientDecoration(),
+//child: Padding(
+//padding: const EdgeInsets.all(16.0),
+//child: Text(
+//"Button",
+//style: TextStyle(color: Colors.white),
+//),
+//),
+//),
+//),
