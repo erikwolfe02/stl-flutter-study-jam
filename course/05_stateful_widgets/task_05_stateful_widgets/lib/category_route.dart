9f01ca1fae5cd0f0e8b26c0208139506f1af81ca
Erik Wolfe <me@erikwolfe.com>
2018-10-30 00:00:00 -0700

    completing task 05

--- course/05_stateful_widgets/task_05_stateful_widgets/lib/category_route.dart
+++ course/05_stateful_widgets/task_05_stateful_widgets/lib/category_route.dart
@@ -16,11 +16,8 @@ final _backgroundColor = Colors.green[100];
 ///
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
-// TODO: Make CategoryRoute a StatefulWidget
-class CategoryRoute extends StatelessWidget {
+class CategoryRoute extends StatefulWidget {
   const CategoryRoute();
-
-  // TODO: Create State object for the CategoryRoute
 
   static const _categoryNames = <String>[
     'Length',
@@ -44,10 +41,31 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  @override
+  CategoryRouteState createState() {
+    return new CategoryRouteState();
+  }
+}
+
+class CategoryRouteState extends State<CategoryRoute> {
+  final categories = <Category>[];
+
+  void initState() {
+    super.initState();
+    for (var i = 0; i < CategoryRoute._categoryNames.length; i++) {
+      categories.add(Category(
+        name: CategoryRoute._categoryNames[i],
+        color: CategoryRoute._baseColors[i],
+        iconLocation: Icons.cake,
+        units: _retrieveUnitList(CategoryRoute._categoryNames[i]),
+      ));
+    }
+  }
+
   /// Makes the correct number of rows for the list view.
   ///
   /// For portrait, we use a [ListView].
-  Widget _buildCategoryWidgets(List<Widget> categories) {
+  Widget _buildCategoryWidgets() {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) => categories[index],
       itemCount: categories.length,
@@ -67,26 +85,10 @@ class CategoryRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Instead of re-creating a list of Categories in every build(),
-    // save this as a variable inside the State object and create
-    // the list at initialization (in initState()).
-    // This way, you also don't have to pass in the list of categories to
-    // _buildCategoryWidgets()
-    final categories = <Category>[];
-
-    for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
-
     final listView = Container(
       color: _backgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: _buildCategoryWidgets(categories),
+      child: _buildCategoryWidgets(),
     );
 
     final appBar = AppBar(
